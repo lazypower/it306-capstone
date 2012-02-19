2cec76d8c3f1607068c640aa271c80dfe9c8fd6a
Charles Butler <chuck@dasroot.net>
2012-02-19 15:54:09 -0500

    Query helper for grabbing the username

---
+++ project/Capstone-csharp/Helpers/HelperQueries.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_csharp.Helpers
+{
+    public class HelperQueries
+    {
+        // method that returns the username as a string.
+        public static string GetUserName(int userID)
+        {
+            if (userID <= 0)
+            {
+                throw new Exception("Passed UserID is not in proper format");
+            }
+
+            // Open the database connection
+            using (var db = new Capstone_csharp.Helpers.DAL.CapstoneEntities())
+            {
+                // Linq has 'navigation properties' - which means YOU DONT HAVE TO WRITE JOINS!
+                // amazing stuff - give it a property to search for - then tell it to select from the entity relation you want
+                var result = from a in db.tUsers
+                           where a.userID == userID // this is our bridge table - tusers. Select by the user ID
+                           select new { a.aspnet_Users.UserName }; // Now bridge that with the asp.net membership provider table, and get the username
+                
+                // the return object was being a pain - so pretend iterate through it
+                foreach (var x in result)
+                {
+                    return x.UserName; // Yep, return the username that appears first. \o/
+                }
+            }
+            return null;
+        }
+
+
+    }
+}
