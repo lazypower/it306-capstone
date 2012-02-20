7d1a7c22030d13002610ddf4095a8c83024e6745
Charles Butler <chuck@dasroot.net>
2012-02-20 01:02:00 -0500

    Removed the script blurb at the bottom and moved it into its own dynamic asset

---
+++ project/Capstone-csharp/Scripts/SiteFunctions.js
@@ -0,0 +1,17 @@
+﻿$('dropdown-toggle').dropdown();
+
+//inline hack to fix the click event on dropdown when it contains a form
+// Thank you stackoverflow 
+$('.dropdown-menu').find('form').click(function (e) {
+    e.stopPropagation();
+});
+
+// Script to remove the form if the user has logged in
+if ($.cookie("Username") != null) {
+    $('#Account-item1').children().remove();
+    $('#AccountLeader').html($.cookie("Username") + " <b class='caret'></b> ");
+    $('#Account-item1').append('<a href="/Account/ChangePassword">Change Password</a>');
+    $('#Account-item1').append('<a href="/Account/Logoff">Log Out</a>');
+}
+        
+       
